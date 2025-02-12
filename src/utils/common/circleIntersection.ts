594a000ed62e95763cfff1918b57bebc5c15ee04
Savan Fami <savanfami1239@gmail.com>
2025-02-12 10:11:22 +0530

    bug fix

--- src/utils/common/circleIntersection.ts
+++ src/utils/common/circleIntersection.ts
@@ -1,13 +1,24 @@
 import { ICircle } from "../../types/type";
 
 
-export const areCirclesIntersecting=(circle1:ICircle,circle2:ICircle):boolean=>{
- const distance=Math.sqrt(
-    Math.pow(circle2.x-circle1.x,2)+Math.pow(circle2.y-circle1.y,2)
- )
+// export const areCirclesIntersecting=(circle1:ICircle,circle2:ICircle):boolean=>{
+//  const distance=Math.sqrt(
+//     Math.pow(circle2.x-circle1.x,2)+Math.pow(circle2.y-circle1.y,2)
+//  )
 
- return distance<circle1.radius+circle2.radius
-}
+//  return distance<circle1.radius+circle2.radius
+// }
+
+export const areCirclesIntersecting = (circle1: ICircle, circle2: ICircle): boolean => {
+    const distance = Math.sqrt(
+        Math.pow(circle2.x - circle1.x, 2) + Math.pow(circle2.y - circle1.y, 2)
+    );
+
+    const sumOfRadii = circle1.radius + circle2.radius;
+    const diffOfRadii = Math.abs(circle1.radius - circle2.radius);
+
+    return distance >= diffOfRadii && distance <= sumOfRadii;
+};
 
 
 export const getRandomRadius=():number=>{
